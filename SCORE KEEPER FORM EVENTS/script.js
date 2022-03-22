401de618c95251aa7a0219801d987ffc93d788fb
Lucas Lobato Botelho <criptoryu@gmail.com>
2022-03-22 22:36:18 +0100

    Code improved and added css classes to winner and loser

--- SCORE KEEPER FORM EVENTS/script.js
+++ SCORE KEEPER FORM EVENTS/script.js
@@ -1,3 +1,5 @@
+/* SELECTORS AND VARIABLES*/
+
 let scoreP1 = 0;
 let scoreP2 = 0;
 
@@ -13,36 +15,52 @@ const spanP2 = document.querySelector('#player2');
 
 
 let bestOf = parseInt(selectBestOf.value);
+let isGameOver = false;
 
 
 
+/* LISTENERS AND FUNCTIONS */
 selectBestOf.addEventListener('change', (e) => {
     bestOf = parseInt(e.target.value);
+    reset();
+    spanP1.textContent = scoreP1;
+    spanP2.textContent = scoreP2;
+    playerOneButton.disabled = false;
+    playerTwoButton.disabled = false;
 });
 
 
 addScore = (playerNum) =>{
-
-    if(playerNum === 1){
-        scoreP1++;
-        spanP1.textContent = scoreP1;
-    } else {
-        scoreP2++;
-        spanP2.textContent = scoreP2;
+    if(!isGameOver){
+        if(playerNum === 1){
+            scoreP1++;
+            spanP1.textContent = scoreP1;
+        } else if(playerNum === 2) {
+            scoreP2++;
+            spanP2.textContent = scoreP2;
+        }
     }
-
 
 
     //MUY IMPORTANTE LA COMPARACIÓN CON ===
-    if(scoreP1 === bestOf || scoreP2 === bestOf){
+    if(scoreP1 === bestOf){
+        isGameOver = true;
         playerOneButton.disabled = true;
         playerTwoButton.disabled = true;
+        spanP1.classList.add('winner');
+        spanP2.classList.add('loser');
 
-        //Añadir clases de winner or loser a spanP1 y spanP2
     }
+    if(scoreP2 === bestOf){
+        isGameOver = true;
+        playerOneButton.disabled = true;
+        playerTwoButton.disabled = true;
+        spanP2.classList.add('winner');
+        spanP1.classList.add('loser');
 
-
+    }
 }
+
 
 
 playerOneButton.addEventListener('click', (e) => {
@@ -57,18 +75,21 @@ playerTwoButton.addEventListener('click', (e) => {
 });
 
 
-resetButton.addEventListener('click', (e) => {
-    e.preventDefault();
+reset = () => {
     scoreP1 = 0;
     scoreP2 = 0;
-
     spanP1.textContent = scoreP1;
     spanP2.textContent = scoreP2;
-
-
+    isGameOver = false;
     playerOneButton.disabled = false;
     playerTwoButton.disabled = false;
+    spanP1.classList.remove('winner','loser');
+    spanP2.classList.remove('winner','loser');
+}
 
-    // Removerle las clases de spanP1 y spanP2
+
+resetButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    reset();
 });
 
